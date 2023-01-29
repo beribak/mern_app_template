4439957765642e2323b7a178ab6600e6dbcba32c
danko beribak <beribakdanko@gmail.com>
2023-01-29 10:59:39 +0100

    first commit static html

---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+
+const router = express.Router();
+
+router.get("/product", (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'product.html'));
+});
+
+router.post("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+  
+
+module.exports = router;
